cd628885b380c87f65e40e6959a954e9a0f489ab
Ilya Ilyin <iyich88@gmail.com>
2024-06-20 11:48:33 +0400

    update src/Games/Even.php

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -2,33 +2,24 @@
 
 namespace Brain\Games\Even;
 
-use function cli\line;
-use function cli\prompt;
+use function Brain\Games\Engine\playGameAndShowResult;
 
 function evenParity()
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-    line('Answer "yes" if the number is even, otherwise answer "no".');
-    for ($i = 0; $i < 3; $i++) {
-        $num = rand(0, 100);
-        line("Question: $num");
-        $answer = prompt('Your answer');
-        if ($num % 2 === 0) {
-            if ($answer === 'yes') {
-                line('Correct!');
-            } else {
-                return line("{$answer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {$name}!");
-            }
-        }
-        if ($num % 2 !== 0) {
-            if ($answer === 'no') {
-                line('Correct!');
-            } else {
-                return line("{$answer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {$name}!");
-            }
-        }
-    }
-    line("Congratulations, %s!", $name);
+    $description = 'Answer "yes" if the number is even, otherwise answer "no".';
+    $gameFunction = function () {
+        [$value, $rightAnswer] = gameFunction();
+        return [$value, $rightAnswer];
+    };
+
+    playGameAndShowResult($description, $gameFunction);
+}
+
+function gameFunction()
+{
+    $min = 0;
+    $max = 99;
+    $value = rand($min, $max);
+    $rightAnswer = $value % 2 === 0 ? 'yes' : 'no';
+    return [$value, $rightAnswer];
 }
