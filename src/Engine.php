d8624aecc9b9e1f5b73ce28d560c29358a885e82
Ilya Ilyin <iyich88@gmail.com>
2024-06-20 11:48:00 +0400

    update src/Engine.php

---
+++ src/Engine.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Brain\Games\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function playGameAndShowResult(string $description, callable $gameFunction)
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line($description);
+    $maxRound = 3;
+    for ($roundNum = 0; $roundNum < $maxRound; $roundNum++) {
+        [$value, $rightAnswer] = $gameFunction();
+        line("Question: $value");
+        $answer = prompt('Your answer');
+        if ($answer === $rightAnswer) {
+            line('Correct!');
+        } else {
+            return line("{$answer} is wrong answer ;(. Correct answer was {$rightAnswer}.\nLet's try again, {$name}!");
+        }
+    }
+    line("Congratulations, %s!", $name);
+}
