1ecb665c78ed4d5d0ec3d030aa8fae54c2252189
Ilya Ilyin <iyich88@gmail.com>
2024-06-20 13:13:39 +0400

    update src/Games/Even.php

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -8,18 +8,12 @@ function evenParity()
 {
     $description = 'Answer "yes" if the number is even, otherwise answer "no".';
     $gameFunction = function () {
-        [$value, $rightAnswer] = gameFunction();
+        $min = 0;
+        $max = 99;
+        $value = rand($min, $max);
+        $rightAnswer = $value % 2 === 0 ? 'yes' : 'no';
         return [$value, $rightAnswer];
     };
 
     playGameAndShowResult($description, $gameFunction);
-}
-
-function gameFunction()
-{
-    $min = 0;
-    $max = 99;
-    $value = rand($min, $max);
-    $rightAnswer = $value % 2 === 0 ? 'yes' : 'no';
-    return [$value, $rightAnswer];
 }
