454c8b034a61223d075a459e042eac13ed741e8b
Ilya Ilyin <iyich88@gmail.com>
2024-07-01 16:31:37 +0400

    add src/Games/Prime.php

---
+++ src/Games/Prime.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Brain\Games\Prime;
+
+use function Brain\Games\Engine\playGameAndShowResult;
+
+function isPrime($number)
+{
+    $devisor = 2;
+    while (($devisor * $devisor <= $number) && ($number % $devisor !== 0)) {
+        $devisor++;
+    }
+    return ($devisor * $devisor > $number);
+}
+
+function primeNumberCheck()
+{
+    $description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    $gameFunction = function () {
+        $min = 2;
+        $max = 100;
+        $givenNumber = random_int($min, $max);
+        $rightAnswer = isPrime($givenNumber) ? 'yes' : 'no';
+        $value = $givenNumber;
+        return [$value, $rightAnswer];
+    };
+
+    playGameAndShowResult($description, $gameFunction);
+}
