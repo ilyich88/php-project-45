51b7b3e11155ca9c07b67c62f00d89b1b4e6d408
ilyich88 <51338481+ilyich88@users.noreply.github.com>
2024-07-02 07:04:51 +0400

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 function playGameAndShowResult(string $description, callable $gameFunction)
 {
-    line("Welcome to the Brain Game!");
+    line("Welcome to the Brain Games!");
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($description);
